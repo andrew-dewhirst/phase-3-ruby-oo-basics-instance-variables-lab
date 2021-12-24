735797fd9926679538d9577b2cf1118e6caefc3e
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-12-24 11:12:59 -0500

    commit tree 6b983534b256a27237a20a6bb4e7d9870979c701

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+  #setter method
+  def name=(dogs_name)
+    #assign the @name instance variable a value of whatever is passed as an argument
+    @this_dogs_name = dogs_name
+  end
+  #getter method
+  def name
+    #access the data from the @name instance variable and return it
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+#call the Dog#name= method with a value of 'Lassie'
+lassie.name = 'Lassie'
+
+puts lassie.name
